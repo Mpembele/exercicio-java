f5b1935d4653b539f60e2313bf188624588bb51e
Mpembele <cardosocosta627@gmail.com>
2022-07-21 14:12:22 -0300

    primeiro commit

---
+++ teste0.2.js
@@ -0,0 +1,16 @@
+/*function conac(str1, str2){
+    return str1 + str2
+}
+conac ('benvindo', 'denovo')
+
+let a = 3;
+let b = 4;
+ function mag(c){
+    c = c + 3;
+    b = 2;
+ }
+ mag(a);
+ console.log('a: ${a}, b: ${b}');*/
+
+ let myVar = 4;
+ myVar = console.log('bom,dia'); 
