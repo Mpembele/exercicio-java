f327ca7fcf76e42826495971d0eb681e4d2ee126
Mpembele <cardosocosta627@gmail.com>
2022-07-24 10:53:48 -0300

    teste0.2.js

--- teste0.2.js
+++ teste0.2.js
@@ -1,16 +1,25 @@
-/*function conac(str1, str2){
-    return str1 + str2
+let nome = prompt("Qual é o seu nome?")
+console.log('nome:', nome)
+
+let sobrenome = prompt("Qual o seu sobre nome?")
+console.log('Sobre nome:',sobrenome)
+
+let idade = prompt("Qual é a sua idade?")
+Number(idade)
+console.log('idade:',idade)
+
+let nome2 = prompt("Qual é o nome da segunda pessoa?")
+console.log('nome:', nome2)
+
+let sobrenome2 = prompt("Qual o sobre nome da segunda pessoa?")
+console.log('Sobre nome:',sobrenome2)
+
+let idade2 = prompt("Qual é a idade da segunda pessoa?")
+Number(idade)
+console.log('idade:',idade2)
+
+if(idade > idade2){
+    console.log(nome,'é mais velho que',nome2)
+}else{
+    console.log(nome2,'é mais velho que',nome)
 }
-conac ('benvindo', 'denovo')
-
-let a = 3;
-let b = 4;
- function mag(c){
-    c = c + 3;
-    b = 2;
- }
- mag(a);
- console.log('a: ${a}, b: ${b}');*/
-
- let myVar = 4;
- myVar = console.log('bom,dia'); 
